dc91f3c3f9b37eabc20bc117a2b6e9ddeb4d8132
Vanadium <vanadium@VanadiumdeMacBook-Air.local>
2025-08-31 18:42:51 +0800

    preset function

--- CCB_Toolkit/MPH/Config.sql
+++ CCB_Toolkit/MPH/Config.sql
@@ -1,2 +1,3 @@
 UPDATE Parameters SET DefaultValue = "0" WHERE ParameterId = "GameMode_Monopolies";
 UPDATE Parameters SET DefaultValue = "0" WHERE ParameterId = "GameMode_SecretSocieties";
+UPDATE Parameters SET DefaultValue = "0" WHERE ParameterId = "TOOLS_COMMAND";
